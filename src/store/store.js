a153d9b3508dcd203190ac30e5568c3d32bf4d23
Osmanov <sinaverosmanovfrontend@gmail.com>
2021-12-14 22:17:16 +0300

    start app

---
+++ src/store/store.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux'
+
+const initialState = {
+    route: '/main',
+    nodes: [],
+    title: 'Main'
+}
+
+function counterReducer(state = { value: 0 }, action) {
+    switch (action.type) {
+        case 'counter/incremented':
+            return { value: state.value + 1 }
+        case 'counter/decremented':
+            return { value: state.value - 1 }
+        default:
+            return state
+    }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(counterReducer)
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// There may be additional use cases where it's helpful to subscribe as well.
+
