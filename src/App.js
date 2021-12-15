baac8ab3eaf9bba9679673c44b1ae1458c2507cd
Osmanov <sinaverosmanovfrontend@gmail.com>
2021-12-15 15:40:09 +0300

    add structure

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ function App() {
   const route = useSelector((state) => state.route);
   console.log(route);
   return (
-    <div className="App">
-      <header className="App-header">start</header>
+    <div className="routing">
+      <header className="routing__header">
+        <div className="routing__header_title">Main</div>
+        <div className="routing__header_button">
+          <button>Перейти к родительскому узлу</button>
+        </div>
+      </header>
+      <div className="routing__content">
+        <div className="routing__content_form">
+          <form action="">
+            <input type="text" />
+            <input type="text" />
+            <button>Добавить узел</button>
+          </form>
+        </div>
+        <div className="routing__content_list">
+          <span>
+            <a href="/main">main&gt;</a>
+          </span>
+
+          <span>
+            <a href="/additional">additional&gt;</a>
+          </span>
+          <span>
+            <a href="/info">info</a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
